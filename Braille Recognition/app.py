a6040737aff759be3738e50dd40b9e18b680e1b8
mattbros <39891692+mattbros@users.noreply.github.com>
2025-04-18 16:26:14 -0500

    Update app.py

--- Braille Recognition/app.py
+++ Braille Recognition/app.py
@@ -67,20 +67,6 @@ def upload():
 def webcam():
     return render_template("webcam.html")
 
-@app.route('/video_feed')
-def video_feed():
-    def generate_frames():
-        cap = cv2.VideoCapture(0)
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                break
-            _, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        cap.release()
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/video_feed')
 def video_feed():
