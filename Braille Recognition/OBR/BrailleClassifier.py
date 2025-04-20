ccebfc16fe6dc30cbc5e8feab479e1847f5ace87
mattbros <39891692+mattbros@users.noreply.github.com>
2025-04-19 23:33:30 -0500

    Update BrailleClassifier.py

--- Braille Recognition/OBR/BrailleClassifier.py
+++ Braille Recognition/OBR/BrailleClassifier.py
@@ -1,76 +1,4 @@
-from math import sqrt
-import cv2
-
-# Import global for optional debug drawing
-try:
-    from app import global_img_debug
-except ImportError:
-    global_img_debug = None
-
-
-def get_distance(p1, p2):
-    x1, y1 = p1
-    x2, y2 = p2
-    return ((x2 - x1) ** 2) + ((y2 - y1) ** 2)
-
-
-def get_dot_nearest(dots, diameter, pt1):
-    nearest = None
-    diameter_squared = diameter ** 2
-    for dot in dots:
-        point = dot[0]
-        dist_from_pt1 = get_distance(point, pt1)
-        if dist_from_pt1 <= diameter_squared:
-            if nearest is None or get_distance(nearest[0], pt1) > dist_from_pt1:
-                nearest = dot
-    return nearest
-
-
-def get_combination(box, dots, diameter):
-    from app import global_img_debug
-
-    result = [0] * 6
-    left, right, top, bottom = box
-    width = right - left
-    height = bottom - top
-
-    # Calculate expected positions of 6-dot Braille grid using relative spacing
-    positions = [
-        (left + 0.25 * width, top + 0.2 * height),   # Dot 1
-        (left + 0.25 * width, top + 0.5 * height),   # Dot 2
-        (left + 0.25 * width, top + 0.8 * height),   # Dot 3
-        (left + 0.75 * width, top + 0.2 * height),   # Dot 4
-        (left + 0.75 * width, top + 0.5 * height),   # Dot 5
-        (left + 0.75 * width, top + 0.8 * height),   # Dot 6
-    ]
-
-    radius_squared = (diameter * 1.2) ** 2  # allow some wiggle room
-
-    for idx, expected_center in enumerate(positions):
-        for dot in dots:
-            dot_center = dot[0]
-            dist = get_distance(dot_center, expected_center)
-            if dist < radius_squared:
-                result[idx] = 1
-                if global_img_debug is not None:
-                    cv2.circle(global_img_debug, expected_center, 6, (0, 255, 0), -1)  # green = hit
-                break
-        else:
-            if global_img_debug is not None:
-                cv2.circle(global_img_debug, expected_center, 6, (0, 0, 255), -1)  # red = miss
-
-    end = (right, (top + bottom) // 2)
-    start = (left, (top + bottom) // 2)
-    return end, start, width, tuple(result)
-
-
-
-def translate_to_number(value):
-    return {
-        'a': '1', 'b': '2', 'c': '3', 'd': '4', 'e': '5',
-        'f': '6', 'g': '7', 'h': '8', 'i': '9', 'j': '0'
-    }.get(value, '?')
-
+from app import get_combination, get_distance
 
 class Symbol:
     def __init__(self, value=None, letter=False, special=False):
@@ -87,6 +15,11 @@ class Symbol:
     def special(self):
         return self.is_special
 
+def translate_to_number(value):
+    return {
+        'a': '1', 'b': '2', 'c': '3', 'd': '4', 'e': '5',
+        'f': '6', 'g': '7', 'h': '8', 'i': '9', 'j': '0'
+    }.get(value, '?')
 
 class BrailleClassifier:
     symbol_table = {
@@ -133,10 +66,10 @@ class BrailleClassifier:
         dots = character.get_dot_coordinates()
         diameter = character.get_dot_diameter()
 
-        end, start, width, combination = get_combination(box, dots, diameter)
-        print("🔢 Dot combination:", combination)
+        end, start, width, combo = get_combination(box, dots, diameter)
+        print("🔢 Dot combination:", combo)
 
-        if combination not in self.symbol_table:
+        if combo not in self.symbol_table:
             self.result += "*"
             return
 
@@ -146,7 +79,7 @@ class BrailleClassifier:
                 self.result += " "
         self.prev_end = end
 
-        symbol = self.symbol_table[combination]
+        symbol = self.symbol_table[combo]
         if symbol.letter() and self.number:
             self.number = False
             self.result += translate_to_number(symbol.value)
