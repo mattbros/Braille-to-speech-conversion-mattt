7040e77402eb44f67e0ab0ef392ee5e57779a8f3
mattbros <39891692+mattbros@users.noreply.github.com>
2025-04-22 16:35:54 -0500

    Update app.py

--- Braille Recognition/app.py
+++ Braille Recognition/app.py
@@ -264,6 +264,22 @@ def upload():
 
     return jsonify({"error": True, "message": "Invalid file format"})
 
+@app.route('/video_feed')
+def video_feed():
+    def gen_frames():
+        cap = cv2.VideoCapture(0)
+        while True:
+            success, frame = cap.read()
+            if not success:
+                break
+            else:
+                ret, buffer = cv2.imencode('.jpg', frame)
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
 if __name__ == "__main__":
     try:
         app.run(debug=True)
